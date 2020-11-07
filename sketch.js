82b378a131a983f1bf728c6dafafa013b00797cb
Kim petare <kim.petare@gmail.com>
2020-11-06 21:00:24 -0500

    created racing game

--- sketch.js
+++ sketch.js
@@ -9,6 +9,8 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1image,car2image,car3image,car4image;
+var trackimage;
 
 
 function setup(){
@@ -28,4 +30,15 @@ function draw(){
     clear();
     game.play();
   }
+  if(gameState === 2){
+    game.end();
+  }
+}
+function preload(){
+  car1image=loadImage("images/car1.png");
+  car2image=loadImage("images/car2.png");
+  car3image=loadImage("images/car3.png");
+  car4image=loadImage("images/car4.png");
+  trackimage=loadImage("images/track.jpg");
+
 }
